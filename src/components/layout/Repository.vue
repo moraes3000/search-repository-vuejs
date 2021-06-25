0304cf3a6ae88cf5c6f5fedb7197139497644104
Bruno Moraes <bruno_bmoraes@hotmail.com>
2021-06-25 14:14:01 -0300

    repository

---
+++ src/components/layout/Repository.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <main id="repositorios">
+      <section v-for="repo in repostorio" v-bind:key="repo.id">
+        <strong> {{ repo.name }}</strong>
+        <p>
+          {{ repo.description }}
+        </p>
+        <span><img src="../../assets/star.png" /> {{ repo.forks }}</span>
+      </section>
+    </main>
+  </div>
+</template>
+
+
+<script>
+// eslint-disable-next-line no-unused-vars
+import axios from 'axios';
+
+export default {
+  name: 'Repositorios',
+  props: ['title'],
+  data() {
+    return {
+      repostorio: null,
+    };
+  },
+  created() {
+    axios
+      .get('https://api.github.com/users/moraes3000/repos')
+      // .get(`https://api.github.com/users/${this.title}/repos`)
+      .then((response) => {
+        this.repostorio = response.data;
+        // this.repos = response;
+        console.log();
+      });
+  },
+};
+</script>
+
+<style  scoped>
+#repositorios {
+  margin-left: 85px;
+}
+#repositorios section {
+  margin-bottom: 37px;
+}
+
+#repositorios strong {
+  color: #000;
+  font-size: 36px;
+  line-height: 42.66px;
+}
+#repositorios p {
+  font-size: 24px;
+  line-height: 28.44px;
+}
+#repositorios span {
+  display: flex;
+  align-items: center;
+}
+</style>
--- src/components/pages/Detalhe.vue
+++ src/components/pages/Detalhe.vue
@@ -1,18 +1,32 @@
 <template>
   <div>
     <Search />
+    <div class="container">   
+      <Aside/>
+      <main id="repositorios">
+        <Repository/>
+      </main>
+    </div>
   </div>
 </template>
 
 <script>
 import Search from "../layout/Search.vue";
+import Repository from '../layout/Repository'
+import Aside from '../layout/Aside.vue'
 export default {
   name: "Detalhe",
   components: {
-    Search,
+    Search,  
+    Aside,  
+    Repository,
   },
 };
 </script>
 
-<style>
+<style  scoped>
+.container {
+  display: flex;
+  margin: 30px;
+}
 </style>
